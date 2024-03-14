8a656f5cd719cb1bf226d433eadf3cfa0ca23bdf
Thomas Downes <u7568037@anu.edu.au>
2024-03-14 12:23:14 +1100

    Cat comment updated

--- src/comp1110/ass2/Cat.java
+++ src/comp1110/ass2/Cat.java
@@ -3,10 +3,8 @@ package comp1110.ass2;
 public class Cat {
     /**Fields
       *colour redCat/blueCat/purpleCat/yellowCat
-      *int[][] position
       *boolean isExhausted
 
       *Methods
-      *move()?
-      *setExhausted?*/
+      *setExhausted()?*/
 }
