0fc310cf474f55262d911dfb2a357ae7c08d35ed
Thomas Downes <u7568037@anu.edu.au>
2024-03-14 11:53:15 +1100

    Created Deck class

---
+++ src/comp1110/ass2/Deck.java
@@ -0,0 +1,10 @@
+package comp1110.ass2;
+
+public class Deck {
+    //Fields
+    //DeckType crossDeck / squareDeck / circleDeck / triangleDeck
+    //List pathwaycards
+
+    //Methods
+    //isEmpty
+}
