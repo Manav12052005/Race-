0b03a7a1960ebbbe03a26d1f2549f5c5f4a1c9ca
Manav Singh <u7782612@anu.edu.au>
2024-03-14 12:17:54 +1100

    Removed Card

--- src/comp1110/ass2/Card.java
+++
@@ -1,7 +0,0 @@
-package comp1110.ass2;
-
-public abstract class Card {
-    /**
-     * The superclass for all card classes.
-     */
-}
--- src/comp1110/ass2/State.java
+++ src/comp1110/ass2/State.java
@@ -1,4 +1,11 @@
 package comp1110.ass2;
 
 public class State {
+    //Fields
+    //Card[] hand;
+
+    //Methods
+    //drawCards(Deck[] decks)
+    //playCard
+    //discardCard
 }
