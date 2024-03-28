c02925b34a912d6ad2498c9120d30d01c8e87d72
Simon <u7761758@anu.edu.au>
2024-03-28 13:33:09 +1100

    Constructed Board and Square classes.

--- src/comp1110/ass2/Board.java
+++ src/comp1110/ass2/Board.java
@@ -1,7 +1,25 @@
 package comp1110.ass2;
 
+import java.util.ArrayList;
+
+/**
+ * holds whole board state (in an ArrayList of squares)
+ */
 public class Board {
-    /**
-     * holds whole board (connected islands)
-     */
+    private ArrayList<Square> squares;
+
+    public Board(String boardString) {
+        ArrayList<Square> squares = new ArrayList<>();
+        boardString.lines().forEach(line -> {
+            int x = 0, y = (squares.size() / line.length()) * 38;
+            int index = 0;
+            for (char ch : line.toCharArray()) {
+                Square current = new Square(x, y, line.charAt(index));
+                squares.add(current);
+                index ++;
+                x += 38;
+            }
+        });
+        this.squares = squares;
+    }
 }
--- src/comp1110/ass2/Square.java
+++ src/comp1110/ass2/Square.java
@@ -1,10 +1,40 @@
 package comp1110.ass2;
 
+/**
+ *determines the possible square conditions
+ *colour
+ *fire
+ *raft
+ */
 public class Square {
-    /**
-     *determines the possible square conditions
-     *colour
-     *fire
-     *raft
-     */
+    private int x;
+    private int y;
+    private enum type {
+        BLUE, GREEN, PURPLE, RED, YELLOW,
+        blueCAT, greenCAT, purpleCAT, redCAT, yellowCAT,
+        FIRE, OBJECTIVE, WILD, wildOCCUPIED, NONE
+    }
+    private type t;
+
+    public Square(int x, int y, char t) {
+        this.x = x;
+        this.y = y;
+        this.t = switch (t) {
+            case 'b' -> type.BLUE;
+            case 'g' -> type.GREEN;
+            case 'p' -> type.PURPLE;
+            case 'r' -> type.RED;
+            case 'y' -> type.YELLOW;
+            case 'B' -> type.blueCAT;
+            case 'G' -> type.greenCAT;
+            case 'P' -> type.purpleCAT;
+            case 'R' -> type.redCAT;
+            case 'Y' -> type.yellowCAT;
+            case 'f' -> type.FIRE;
+            case 'o' -> type.OBJECTIVE;
+            case 'w' -> type.WILD;
+            case 'W' -> type.wildOCCUPIED;
+            default -> type.NONE;
+        };
+    }
 }
