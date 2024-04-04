dfec167f128666d7324128327fe3daceda0e43cc
Simon <u7761758@anu.edu.au>
2024-04-04 18:51:26 +1100

    Added constructor for Cat

--- src/comp1110/ass2/Cat.java
+++ src/comp1110/ass2/Cat.java
@@ -1,13 +1,20 @@
 package comp1110.ass2;
 
 public class Cat {
-
     private String colour;
-
     private Location location;
+    private boolean exhausted;
+
+    // Constructor
+    public Cat(String colour, Location location){
+        this.colour = colour;
+        this.location = location;
+        this.exhausted = false;
+    }
     public boolean isExhausted(){
         return false;
     }
     public boolean setExhausted(){
         return false;
-    }}
+    }
+}
