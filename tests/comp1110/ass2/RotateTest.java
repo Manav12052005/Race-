90f425fd1a5d10cb1433c0765a45002d811bf6d5
Thomas Downes <u7568037@anu.edu.au>
2024-04-19 12:37:31 +1000

    RotateTest created

---
+++ tests/comp1110/ass2/RotateTest.java
@@ -0,0 +1,10 @@
+package comp1110.ass2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+public class RotateTest {
+    //tests whether the FireTile and PathwayCard rotate function works properly
+
+}
